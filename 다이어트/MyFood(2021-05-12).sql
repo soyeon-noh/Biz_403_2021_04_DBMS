26cffd34ea415ad9fd60e7c0e49a1cfabd2647ee
soyeon-noh <1010qwerty12@gmail.com>
2021-05-12 16:42:37 +0900

    food with

--- 다이어트/MyFood(2021-05-12).sql
+++ 다이어트/MyFood(2021-05-12).sql
@@ -13,3 +13,152 @@ CREATE TABLE tbl_foods(
     fd_carbo	NUMBER(10,2),		
     fd_sugar	NUMBER(10,2)	
 );		
+
+CREATE TABLE tbl_company(
+    cp_code	CHAR(6)		PRIMARY KEY,
+    cp_name	nVARCHAR2(125)	NOT NULL,
+    cp_ceo	nVARCHAR2(50),
+    cp_tel	VARCHAR2(20),
+    cp_addr	nVARCHAR2(125),
+    cp_item	nVARCHAR2(30)
+);
+
+CREATE TABLE tbl_items(
+    it_code	CHAR(4)		PRIMARY KEY,
+    it_name	nVARCHAR2(30)	NOT NULL	
+);
+
+
+CREATE TABLE tbl_myfoods(
+    mf_seq	NUMBER		PRIMARY KEY,
+    mf_date	VARCHAR2(10)	NOT NULL,
+    mf_fcode	CHAR(7)	NOT NULL,
+    mf_amt	NUMBER(10,2)	NOT NULL
+);
+
+-- 시퀀스 만들기
+CREATE SEQUENCE seq_myfoods
+START WITH 1 INCREMENT BY 1;
+
+-- import 된 데이터 개수 확인
+SELECT COUNT(*) FROM tbl_foods;
+SELECT COUNT(*) FROM tbl_company;
+SELECT COUNT(*) FROM tbl_items;
+
+-- 식품정보와 제조자정보를 JOIN 하여
+-- 데이터가 정상적으로 import 되었는지 확인
+--  여기서 null 값이 하나라도 나오면 안된다. 
+--  (나오면 foreign key 설정이 안됨)
+SELECT *
+FROM tbl_foods F
+    LEFT JOIN tbl_company C
+        ON F.fd_ccode = C.cp_code
+    WHERE C.cp_code IS NULL;
+
+-- 식품정보와 Item정보를 JOIN
+SELECT *
+FROM tbl_foods F
+    LEFT JOIN tbl_items I
+        ON F.fd_icode = I.it_code
+    WHERE I.it_code IS NULL;
+    
+    
+-- 세개의 table 간에 참조무결성 설정
+-- 누가 부모이고, 누가 자손인지 파악
+-- 자손 : tbl_foods, 부모 : tbl_company, tbl_items
+-- FK 설정은 자손 table에 설정하기
+
+ALTER TABLE tbl_foods
+ADD CONSTRAINT fk_company
+FOREIGN KEY(fd_ccode)
+REFERENCES tbl_company(cp_code);
+
+ALTER TABLE tbl_foods
+ADD CONSTRAINT fk_item
+FOREIGN KEY(fd_icode)
+REFERENCES tbl_items(it_code);
+
+CREATE VIEW view_식품정보 AS(
+SELECT
+    fd_code	    식품코드,
+    fd_name	    식품명,
+    fd_year	    출시연도,
+    fd_once	    제공량,
+    fd_capa	    총내용량,
+    fd_cal	    에너지,
+    fd_protein  단백질,
+    fd_fat  	지방,
+    fd_carbo	탄수화물,
+    fd_sugar	총당류,
+    cp_code	제조사코드,
+    cp_name	제조사명,
+    cp_ceo	대표,
+    cp_tel	대표전화,
+    cp_addr	주소,
+    cp_item	주요품목,
+    it_code	    분류코드,
+    it_name	    분류명
+FROM tbl_foods
+    LEFT JOIN tbl_company
+        ON fd_ccode = cp_code
+    LEFT JOIN tbl_items
+        ON fd_icode = it_code);
+
+CREATE VIEW view_섭취정보 AS(
+    SELECT 
+        mf_seq	일련번호,
+        mf_date	섭취일자,
+        mf_fcode	식품코드,
+        mf_amt	섭취량,
+    
+        식품명,
+        출시연도,
+        제조사코드,
+        분류코드,
+        제공량,
+        총내용량,
+        에너지,
+        단백질,
+        지방,
+        탄수화물,
+        총당류,
+        제조사명,
+        대표,
+        대표전화,
+        주소,
+        주요품목,
+        분류명
+    FROM tbl_myfoods
+        LEFT JOIN view_식품정보
+            ON mf_fcode = 식품코드
+);
+        
+INSERT INTO tbl_myfoods(mf_seq, mf_fcode, mf_date, mf_amt)
+VALUES(seq_myfoods.NEXTVAL, 'PD00001', '2021-05-12', 2);
+
+SELECT *
+FROM view_섭취정보;
+
+
+-- 
+CREATE VIEW view_섭취량계산 AS(
+    SELECT 
+        mf_date 	섭취일자,
+        mf_fcode    식품코드,
+        식품명,
+        mf_amt	    섭취량,
+        제공량 * mf_amt AS 제공량,
+        총내용량 * mf_amt AS 총내용량,
+        에너지 * mf_amt AS 에너지,
+        단백질 * mf_amt AS 단백질,
+        지방 * mf_amt AS 지방,
+        탄수화물 * mf_amt AS 탄수화물,
+        총당류 * mf_amt AS 총당류
+   
+    FROM tbl_myfoods
+        LEFT JOIN view_식품정보
+            ON mf_fcode = 식품코드
+);
+
+SELECT * FROM view_식품정보
+WHERE 식품명 LIKE '%치즈%';
