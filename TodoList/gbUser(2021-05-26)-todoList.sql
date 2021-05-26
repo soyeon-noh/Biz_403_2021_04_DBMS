3341b67c0a548f84469b0fe3d0fd0a74226cbb51
soyeon-noh <1010qwerty12@gmail.com>
2021-05-26 16:45:24 +0900

    학원 todo

---
+++ TodoList/gbUser(2021-05-26)-todoList.sql
@@ -0,0 +1,13 @@
+SHOW DATABASES;
+
+CREATE DATABASE myDB;
+USE myDB;
+CREATE TABLE tbl_todolist (
+	td_seq BIGINT auto_increment PRIMARY KEY,
+	td_sdate VARCHAR(10) NOT NULL, -- 추가된 날짜
+    td_stime VARCHAR(10) NOT NULL, -- 추가된 시간
+    td_doit VARCHAR(300) NOT NULL, 
+    
+    td_edate VARCHAR(10) DEFAULT '', -- 완료 날짜, 디폴트값 NULL이 아닌 ''으로 지정
+    td_etime VARCHAR(10) DEFAULT '' -- 완료 시간
+);
