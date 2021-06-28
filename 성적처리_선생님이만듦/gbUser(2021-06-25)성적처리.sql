ea9f742333cf5f3e4c7d800fbd8d026fbd5d44de
soyeon-noh <1010qwerty12@gmail.com>
2021-06-28 16:47:08 +0900

    추가

--- 성적처리_선생님이만듦/gbUser(2021-06-25)성적처리.sql
+++ 성적처리_선생님이만듦/gbUser(2021-06-25)성적처리.sql
@@ -63,3 +63,57 @@ WHERE sc_stnum = '20210001';
 
 DELETE FROM tbl_score
 WHERE sc_stnum = '20210001';
+
+
+-- 6/28 --------------------------------------
+
+DROP table tbl_score;
+/*
+TABLE에 
+	INSERT INTO ON DUPLICATE KEY UPDATE를
+	실행하기 위해서는 PK 설정을 변경해야 한다.
+tbl_score는 두개의 칼럼을 기준으로
+	UPDATE, DELETE 를 수행해야하는 문제가 발생한다.
+가장 좋은 설계는 UPDATE, DELETE 를 수행할 때
+	한개의 칼럼으로 구성된 PK를 기준으로 
+    수행하는 것이다.
+    
+*/
+CREATE TABLE tbl_score (
+	-- sc_seq BIGINT AUTO_INCREMENT PRIMARY KEY,
+    sc_stnum char(8) NOT NULL,
+    sc_sbcode char(4) NOT NULL,
+    sc_score INT NOT NULL,
+    PRIMARY KEY(sc_stnum, sc_sbcode) -- 슈퍼키로 칼럼선언
+);
+
+/* 방법2
+PK는 그대로 살려두고
+두개의 칼럼을 묶어 UNIQUE로 설정
+두개 칼럼의 값이 동시에 같은 경우엔 추가하지 말라는
+제약조건 설정
+*/
+
+CREATE TABLE tbl_score (
+	sc_seq BIGINT AUTO_INCREMENT PRIMARY KEY,
+    sc_stnum char(8) NOT NULL,
+    sc_sbcode char(4) NOT NULL,
+    sc_score INT NOT NULL,
+    UNIQUE(sc_stnum, sc_sbcode) -- double unique! 
+);
+
+
+
+SELECT * FROM tbl_score;
+SELECT * FROM tbl_subject;
+
+
+-- 한 학생의 3과목 점수를 개별적으로 INSERT하기
+INSERT INTO tbl_score(sc_stnum, sc_sbcode, sc_score) VALUES ('20210002', 'S001', 90);
+INSERT INTO tbl_score(sc_stnum, sc_sbcode, sc_score) VALUES ('20210002', 'S002', 95);
+INSERT INTO tbl_score(sc_stnum, sc_sbcode, sc_score) VALUES ('20210002', 'S003', 92);
+
+-- 1번의 INSERT 명령문으로 다수의 명령문 INSERT하기
+INSERT INTO tbl_score (sc_stnum, sc_sbcode, sc_score)  
+VALUES ('20210003', 'S001', 90), ('20210003', 'S002', 95), ('20210003', 'S003', 92);
+SELECT * FROM tbl_score;
