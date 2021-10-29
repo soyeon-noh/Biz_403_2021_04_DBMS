8e4583dacdc923b357817e2712085c5a6c8c8f4d
soyeon-noh <1010qwerty12@gmail.com>
2021-10-29 16:45:19 +0900

    naraDB

---
+++ MySQL/naraDB(2021-10-27).sql
@@ -0,0 +1,13 @@
+-- naraDB Schema 생성하기
+CREATE DATABASE naraDB;
+USE naraDB;
+show tables;
+DESC tbl_book;
+DESC tbl_readbook;
+
+SELECT * FROM tbl_book;
+SELECT * FROM tbl_readbook;
+
+-- 2021-10-29
+SELECT * FROM tbl_books;
+SELECT * FROM tbl_readbooks;
